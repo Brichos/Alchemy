fa1a37721f676e356c513c3630ab3dd4a7734183
Brichos <nicholasak@uia.no>
2026-03-16 23:24:46 +0100

    added explanation to how i solved the task

--- tests.mjs
+++ tests.mjs
@@ -4,3 +4,10 @@ console.log(number+number);
 
 //task 2
 console.log(Math.PI);
+
+//Task 3
+//Found on https://en.wikipedia.org/wiki/Planetary_symbols, i don't know how i'd solve it with code
+
+//task 4
+/* The poem has capital letters: "Still flows the Icy Lethe, Veiling all 'neath Eldritch Rime."
+S I L V E R, answer is SILVER*/
